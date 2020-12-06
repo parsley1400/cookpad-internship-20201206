d833a192bb56e24a9e307b911bfadea0ee57e538
parsley1400 <topthecap@gmail.com>
2020-12-06 16:31:10 +0900

    title

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require_relative 'lib/poll'
+require_relative 'lib/vote'
 
 get '/' do
-  [200, {}, '投票一覧']
+  '投票一覧'
+  polls = [
+    Poll.new('好きな料理', ['肉じゃが', '生姜焼き', '唐揚げ'], '20201220'),
+    Poll.new('人気投票', ['おむすびけん', 'クックパッドたん'], '20201220')
+  ]
+
+  erb :index, locals:{ polls: polls }
 end
